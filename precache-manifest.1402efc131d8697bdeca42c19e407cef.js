723ae38ae18d77d7e7c1f4f4b850f4f52f241ef5
lorenzoricci <lorenzo@amalgama.co>
2019-09-06 15:37:58 -0300

    Updates

---
+++ precache-manifest.1402efc131d8697bdeca42c19e407cef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fde5aeb4ec81faacae6479f9c9d767a",
+    "url": "/twitchClone/index.html"
+  },
+  {
+    "revision": "3131d30bcdf530884ade",
+    "url": "/twitchClone/static/js/2.ff450828.chunk.js"
+  },
+  {
+    "revision": "5325989e07010315d99c",
+    "url": "/twitchClone/static/js/main.9580d714.chunk.js"
+  },
+  {
+    "revision": "c885fef5adb399bf662e",
+    "url": "/twitchClone/static/js/runtime~main.8f0655b2.js"
+  }
+]);
